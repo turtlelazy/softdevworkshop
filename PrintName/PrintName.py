4e80b4fab0b948d52ea81358d5c236fe5ec1a3c7
turtlelazy <ishraqmahid@gmail.com>
2021-09-23 01:28:19 -0400

    heading

--- PrintName/PrintName.py
+++ PrintName/PrintName.py
@@ -1,3 +1,8 @@
+#Ishraq Mahid
+#SoftDev
+#K02 -- PrintName Classwork
+#2021-09-21
+
 import random
 pd1 = ["Ivan", "Gavin", "Ishraq", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth"]
 pd2 = ["James", "Robert", "John", "Donald", "Steven", "Paul", "Andrew",  "Joshua", "Maria", "Nushi", "Mohammed", "Jose", "Muhammad", "Mohamed", "Wei", "Mohammad", "Ahmed", "Yan", "Ali", "Li", "Abdul", "Ana"]
