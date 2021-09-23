d89a0cc3a70eae909c807c1f65fe983e9027c91e
turtlelazy <ishraqmahid@gmail.com>
2021-09-23 01:23:26 -0400

    bug fix

---
+++ PrintName/PrintName.py
@@ -0,0 +1,17 @@
+import random
+pd1 = ["Ivan", "Gavin", "Ishraq", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth"]
+pd2 = ["James", "Robert", "John", "Donald", "Steven", "Paul", "Andrew",  "Joshua", "Maria", "Nushi", "Mohammed", "Jose", "Muhammad", "Mohamed", "Wei", "Mohammad", "Ahmed", "Yan", "Ali", "Li", "Abdul", "Ana"]
+
+def printName():
+    if random.randint(0,1) > 0:
+        print(randomStudent(pd1))
+    else:
+        print(randomStudent(pd2))
+    
+            
+def randomStudent(periodList):
+    return periodList[random.randint(0,len(periodList)-1)]
+
+printName()
+#if randint(len(pd1),
+#print(printname)
