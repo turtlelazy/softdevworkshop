32e981a6271604e871113309dc8702c265efaca9
turtlelazy <ishraqmahid@gmail.com>
2021-10-07 20:06:39 -0400

    12_flask

---
+++ 12_flask-jinja/CoolDogsApp.py
@@ -0,0 +1,29 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2021
+
+# Q0: What happens if you remove render_template from this line?
+#Nothing happens to the root directory. But it would cause an error on 
+# /my_foist_template, because the template can't be rendered
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+names = ["Ishraq & Fish", "Gavin & Craig", "Ivan & Bob"]
+
+@app.route("/cooldogsonly") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
+#Yes. We've realized that this keyword specifies the directory, with root being at /
+def test_tmplt():
+    return render_template( 'model_tmplt.html', foo="fooooo", names=names) #Q2: What is the significance of each argument?
+    #They look like parameters to this function called render_template. 
+    #We specify the target template, and set variables equal to what we want.
+    #The template itself has spots for the foo and collection variables, 
+    #and it appears almost as if that spot is translated by Flask for it's own purposes.
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
