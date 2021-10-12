e57d4604e749f6e50be4e26985f2b4a2ff8c904d
turtlelazy <ishraqmahid@gmail.com>
2021-10-11 22:24:10 -0400

    full notes and code and good work

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -3,6 +3,7 @@
 # K13 -- Template for Success
 # 2021-10-08
 
+#Initial setup
 import occupations
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -12,11 +13,15 @@ coll = occupations.init()
 def test_tmplt():
 
     return render_template('tablified.html', 
+        #foo is the website heading
         foo="Occupations", 
+        #header is the h1 tag
         header = "Data Covering Various Occupations and their Respective Percentages", 
+        #Names is our trio
         names = "Gitsters | Alejandro Alonso, Ivan Lam, Ishraq Mahid",
+        #occ is a random occupation
         occ = occupations.chooseRandom()
-,
+,       #taken from occupations.py, it is essentially a dictionary being passed on
         collection=coll)
 
 if __name__ == "__main__":
