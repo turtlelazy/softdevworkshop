73c3858637f6495ec17842d8dad81ee4ed43132b
turtlelazy <ishraqmahid@gmail.com>
2021-10-17 21:09:27 -0400

    mathcing partner's work

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,82 +1,44 @@
 """
-Ishraq Mahid
+Ishraq Mahid, Lucas Tom-Wong (LTW), Tomas Acuna
 SoftDev
 K14 -- Form and Function -- Utilizing Request Info
 2021-10-15
 """
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+from flask import Flask  # facilitate flask webserving
+from flask import render_template  # facilitate jinja templating
+from flask import request  # facilitate form submission
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
-app = Flask(__name__)    #create Flask object
+app = Flask(__name__)  # create Flask object
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not. Can you predict which?
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests. Process results.
-PROTIP: Insert your own in-line comments wherever they will help your future self and/or current teammates understand what is going on.
-'''
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    # print("\n\n\n")
-    # print("***DIAG: this Flask obj ***")
-    # print(app)
-    # print("***DIAG: request obj ***")
-
-    #Request objects predicted to not work because there is not request going to the template html
-
-    # print(request)
-    # print("***DIAG: request.args ***")
-    # print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    # print("***DIAG: request.headers ***")
-    # print(request.headers)
-
-    #default login template
-
-    """login template, which was provided"""
-    return render_template( 'login.html' )
+    """
+    renders the login page
+    """
+    return render_template('login.html')
+    #brings up the login.html page
+    #askes for inputs of a text and to press a submit button
 
 
 @app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-    #Request objects predicted to print based on what was received from the previous website, as there exists a request object 
-    #to be passed.
-    # print("\n\n\n")
-    # print("***DIAG: this Flask obj ***")
-    # print(app)
-    # print("***DIAG: request obj ***")
-    # print(request.method)
-    # print("***DIAG: request.args ***")
-    # print(request.args['username'])
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-
-    #request has many different attribtues within it that we can utilize as a string
-    # print("***DIAG: request.headers ***")
-    # print(request.headers)
+    """
+    renders response template, placing in the username and method retrieved from the request
+    """
+    return render_template('response.html', username=request.args['username'], method=request.method)
+    #brings up the response.html page with username and the method request sent to the page
+    #uses responses from previous inputs/method
 
 
-    """inputs in the username and request method
-    username is derieved from the GET request, username attribute; method is derived from request method type"""
-
-    return render_template('response.html', username = request.args.get('username'), method = request.method)
-    #return "Hello " + request.args['username'] + "! It's time to get Flasky!" + "\n" + "You made a " + request.method + " request just now." # response to a form submission
-    #return str(request)
-
-
-    
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+if __name__ == "__main__":  
+    """
+    false if this file imported as module
+    debugging enabled    
+    """
+    app.debug = True
     app.run()
