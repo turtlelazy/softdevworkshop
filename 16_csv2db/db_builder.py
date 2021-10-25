33bf6a49107f15650cffac691dba4ad8029eed45
turtlelazy <ishraqmahid@gmail.com>
2021-10-25 00:58:22 -0400

    commentified, headified, oldified, and debugified

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,19 +1,14 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+#The Onions - Tomas Acuna, Lucas Tom-Wong, Ishraq Mahid
+#SoftDev
+#k16 -- SQLiters and Database Agony
+#2021 - 10 - 25
+#2.5 hours
 
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitate CSV I/O
+import sqlite3  # enable control of an sqlite database
+import csv  # facilitate CSV I/O
 
-
-DB_FILE="discobandit.db"
-
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
-#print statements trigger based on debug
-debug = False
+#debug statements will print
+debug = True
 #==========================================================
 
 
@@ -21,101 +16,105 @@ debug = False
 
 #code to read in roster info
 
-def readFile(file):
-    '''
-    returns file as a dictionary seperated by rows
-    '''
-    return csv.DictReader(open(file))
+def list_to_string(lst):
+    return "(" + ", ".join(lst) + ")"
 
-#all dicts will have the same headers since both csv files have the same headers
-def dict2SQ(dict, dict_name):
+
+def append(a, b):
+    return a + " " + b
+
+
+def add_format(value, field_type):
+    if field_type == "TEXT":
+        return "'" + value + "'"
+    return value
+
+
+def dict2SQ(dict_reader, db, table_name, field_types):
     '''
     converts a CSV dictinoary to an sqlite table
+    dict_reader is dictionary
+    db is sqlite connection
+    table_name is desired name
+    field_types are inputted as a parameter
     '''
+    c = db.cursor()
+    field_names = dict_reader.fieldnames
 
-    #headers variable types are the same for both csv files
-    dict_headers = " (name TEXT, num0 INTEGER, num1 INTEGER)"
-    c.execute("CREATE TABLE IF NOT EXISTS " + dict_name + dict_headers)
+    headers = map(append, field_names, field_types)
+    header_string = list_to_string(headers)
+    c.execute("CREATE TABLE IF NOT EXISTS " + table_name + " " + header_string)
 
-    #csv header returned by .fieldnames
-    dict_loop = dict.fieldnames
+    for dic in dict_reader:
+        values = map(dic.get, field_names)
+        formatted_values = map(add_format, values, field_types)
+        value_string = list_to_string(formatted_values)
+        c.execute("INSERT INTO " + table_name + " VALUES " + value_string)
 
-    #loop through items and add them to the table; set debug to true to view the string of items added
-    for item in dict:
-        item_string = "('" + item[dict_loop[0]] + "'," + \
-            item[dict_loop[1]] + "," + item[dict_loop[2]] + ")"
-        c.execute("INSERT INTO "+ dict_name +" VALUES "+item_string+";")
-        if debug:
-            print(item_string)
-    
     db.commit()
 
-    
-    # for item in dict:
-    #     item_string = "('" + dict[item][dict_loop[0]] + "', " 
-    #     + dict[item][dict_loop][1]+ "," + dict[item][dict_loop][2] +")"
+    # for value in dict:
+    #     value_string = "('" + dict[value][dict_loop[0]] + "', "
+    #     + dict[value][dict_loop][1]+ "," + dict[value][dict_loop][2] +")"
     #     if debug:
-    #         print(item_string)
-        #c.execute("INSERT INTO roster VALUES ('whose-it', 2);")
-        
-    #c.execute("CREATE TABLE [IF NOT EXISTS] " + dict_name + dict_headers)
+    #         print(value_string)
+    #c.execute("INSERT INTO roster VALUES ('whose-it', 2);")
 
-def printDictioanry(dic):
-    '''
-    prints header of each item in dictionary - turned out to be less useful than intended
-    '''
-    for row in dic:
-        print(", ".join(row))
+    #c.execute("CREATE TABLE [IF NOT EXISTS] " + table_name + table_headers)g
 
 
-def printDB(tableName):
+def printDB(db, table_name):
     '''
     prints the sqliteDB given tablename to select it
+    db is sqlite connection
+    table_name is name of target table
     '''
+    if dbExistence(db, table_name):
+        c = db.cursor()
+        c.execute("SELECT * FROM " + table_name)
+        rows = c.fetchall()
+        for row in rows:
+            print(row)
+    else:
+        print("table '" + table_name + "' does not exist")
 
-    #holds the info from SELECT in the curson and then fetches it, placing it into rows
-    c.execute("SELECT * FROM " + tableName)
-    rows = c.fetchall()
 
-    #print each item in rows
-    for item in rows:
-        print(item)
-
-
-#source: https://www.sqlitetutorial.net/sqlite-exists/ 
-def dbExistence(tableName):
+def dbExistence(db, table_name):
     '''
     returns boolean based on whether or not the given tableName exists
     '''
-
-    #holds data on existence of tableName
-    c.execute("SELECT EXISTS(SELECT * FROM '"+tableName+"')")
-
-    #output of EXISTS is 0 or 1; converts value of exists output into bool
-    exists = c.fetchone()[0]
-    exists = exists == 1
-    return(exists)
-
+    c = db.cursor()
+    c.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='" + table_name + "'")
+    return(bool(c.fetchone()))
 #command = ""          # test SQL stmt in sqlite3 shell, save as string
 #c.execute(command)    # run SQL statement
 
 #==========================================================
 
+#runs if not imported
 if __name__ == "__main__":
-    #runs if not imported
+    # open if file exists, otherwise create
+    discobandit = sqlite3.connect("discobandit.db")
 
-    rosterDict = readFile('students.csv')
-    #dict2SQ(rosterDict, "roster")
-    #db.commit()  # save changes
-    printDB("roster")
-    print(dbExistence('roster'))
-    # coursesDict = readFile('courses.csv')
-    # dict2SQ(coursesDict, "courses")
-    # db.commit()  # save changes
-    # printDB("courses")
-    db.close()  # close database
-    
-    
+    #opens using target file and runs dict2SQ with chosen parameters
+    with open("students.csv") as students:
+        roster_reader = csv.DictReader(students)
+        dict2SQ(roster_reader, discobandit, "roster",
+                ["TEXT", "INTEGER", "INTEGER"])
+    with open("courses.csv") as courses:
+        gradebook_reader = csv.DictReader(courses)
+        dict2SQ(gradebook_reader, discobandit, "gradebook",
+                ["TEXT", "INTEGER", "INTEGER"])
 
+    #test statements
+    if debug:
+        print("roster:")
+        printDB(discobandit, "roster")
+        print("gradebook:")
+        printDB(discobandit, "gradebook")
 
+        print("cheese:")
+        printDB(discobandit, "cheese") #should not exist
 
+    discobandit.close()  # close database
---
+++ 16_csv2db/db_builderOLD.py
@@ -0,0 +1,122 @@
+#The Onions - Tomas Acuna, Lucas Tom-Wong, Ishraq Mahid
+#SoftDev
+#k16 -- SQLiters and Database Agony
+#2021 - 10 - 25
+#2.5 hours
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#print statements trigger based on debug
+debug = False
+#==========================================================
+
+
+# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+
+#code to read in roster info
+
+def readFile(file):
+    '''
+    returns file as a dictionary seperated by rows
+    '''
+    return csv.DictReader(open(file))
+
+#all dicts will have the same headers since both csv files have the same headers
+def dict2SQ(dict, dict_name):
+    '''
+    converts a CSV dictinoary to an sqlite table
+    '''
+
+    #headers variable types are the same for both csv files
+    dict_headers = " (name TEXT, num0 INTEGER, num1 INTEGER)"
+    c.execute("CREATE TABLE IF NOT EXISTS " + dict_name + dict_headers)
+
+    #csv header returned by .fieldnames
+    dict_loop = dict.fieldnames
+
+    #loop through items and add them to the table; set debug to true to view the string of items added
+    for item in dict:
+        item_string = "('" + item[dict_loop[0]] + "'," + \
+            item[dict_loop[1]] + "," + item[dict_loop[2]] + ")"
+        c.execute("INSERT INTO "+ dict_name +" VALUES "+item_string+";")
+        if debug:
+            print(item_string)
+    
+    db.commit()
+
+    
+    # for item in dict:
+    #     item_string = "('" + dict[item][dict_loop[0]] + "', " 
+    #     + dict[item][dict_loop][1]+ "," + dict[item][dict_loop][2] +")"
+    #     if debug:
+    #         print(item_string)
+        #c.execute("INSERT INTO roster VALUES ('whose-it', 2);")
+        
+    #c.execute("CREATE TABLE [IF NOT EXISTS] " + dict_name + dict_headers)
+
+def printDictioanry(dic):
+    '''
+    prints header of each item in dictionary - turned out to be less useful than intended
+    '''
+    for row in dic:
+        print(", ".join(row))
+
+
+def printDB(tableName):
+    '''
+    prints the sqliteDB given tablename to select it
+    '''
+
+    #holds the info from SELECT in the curson and then fetches it, placing it into rows
+    c.execute("SELECT * FROM " + tableName)
+    rows = c.fetchall()
+
+    #print each item in rows
+    for item in rows:
+        print(item)
+
+
+#source: https://www.sqlitetutorial.net/sqlite-exists/ 
+def dbExistence(tableName):
+    '''
+    returns boolean based on whether or not the given tableName exists
+    '''
+
+    #holds data on existence of tableName
+    c.execute("SELECT EXISTS(SELECT * FROM '"+tableName+"')")
+
+    #output of EXISTS is 0 or 1; converts value of exists output into bool
+    exists = c.fetchone()[0]
+    exists = exists == 1
+    return(exists)
+
+#command = ""          # test SQL stmt in sqlite3 shell, save as string
+#c.execute(command)    # run SQL statement
+
+#==========================================================
+
+if __name__ == "__main__":
+    #runs if not imported
+
+    rosterDict = readFile('students.csv')
+    #dict2SQ(rosterDict, "roster")
+    #db.commit()  # save changes
+    printDB("roster")
+    print(dbExistence('roster'))
+    # coursesDict = readFile('courses.csv')
+    # dict2SQ(coursesDict, "courses")
+    # db.commit()  # save changes
+    # printDB("courses")
+    db.close()  # close database
+    
+    
+
+
+
