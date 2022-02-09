29c210ee7fc3c17f2c8b3ca40cebcb8dd02b67c3
turtlelazy <ishraqmahid@gmail.com>
2022-02-09 06:38:18 -0500

    pushery

---
+++ k28_jsdom/dommy_consolery.js
@@ -0,0 +1,71 @@
+// Team Oranges :: Ishraq Mahid, Julia Nelson 
+// SoftDev pd0
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2022-02-08t
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-J in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+//adds item to the tlist
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+//removes the item at index n
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  //weird way of defining a remove method   
+  listitems[n].remove();
+};
+
+//turns the color of the last item in the list to red
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+//stripes the items that do not already have a color
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+//insert your implementations here for...
+// FIB
+// FAC
+// GCD
